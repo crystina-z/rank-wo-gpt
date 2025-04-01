45c5774b423d34a99b38ee8e848ab7a93c69c574
Crystina <x978zhan@uwaterloo.ca>
2025-04-01 17:19:23 -0400

    eval

--- inference/evaluate.py
+++ inference/evaluate.py
@@ -1,5 +1,7 @@
 import ir_measures
 from ir_measures import *
+import ir_datasets
+
 from nirtools.ir import load_runs
 
 from pprint import pprint
@@ -9,7 +11,8 @@ METRICS = [nDCG@10, R@100]
 
 def evaluate(runfile, dataset_name):
     reranked_runs = load_runs(runfile)
-    results = ir_measures.calc_aggregate(METRICS, dataset_name, reranked_runs)
+    qrels = ir_datasets.load(dataset_name).qrels_iter()
+    results = ir_measures.calc_aggregate(METRICS, qrels, reranked_runs)
 
     print(f"Evaluating file {runfile} on dataset {dataset_name}:")
     pprint(results)
